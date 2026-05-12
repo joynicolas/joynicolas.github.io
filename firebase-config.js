2ce0e755d111963399307e59111a8f0a1f3e532e
joynicolas <34314171+joynicolas@users.noreply.github.com>
2026-05-13 01:01:49 +0530

    Update firebase-config.js
    config details added

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // firebase-config.js
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDFWSUjjun9wLDWUj2AJqPn_9SS_EojJ88",
+  authDomain: "portfoliolikes-b710e.firebaseapp.com",
+  projectId: "portfoliolikes-b710e",
+  storageBucket: "portfoliolikes-b710e.firebasestorage.app",
+  messagingSenderId: "546982146584",
+  appId: "1:546982146584:web:233d61bb63053e29b5496d"
 };
